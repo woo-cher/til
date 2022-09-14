dd90c79144c115cfc4e1ea47b5d526eebd6782e2
lminggvick <lminggvick@naver.com>
2022-09-14 13:51:59 +0900

    external: kafka & rabbitmq - #12

--- src/main/java/com/study/til/TilApplication.java
+++ src/main/java/com/study/til/TilApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:env.properties")
 public class TilApplication {
 
     public static void main(String[] args) {
